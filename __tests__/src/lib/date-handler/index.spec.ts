2d98edcad4fe7f180540424b4978bd605a74aac8
indominuShot <luang193@gmail.com>
2022-04-29 22:13:43 -0300

    feat: ensure sut not thows with a valid date

--- __tests__/src/lib/date-handler/index.spec.ts
+++ __tests__/src/lib/date-handler/index.spec.ts
@@ -8,7 +8,7 @@ class DateHandler {
   private __initHandler(date?: string | Date | number) {
     if (!date) return new Date();
 
-    const isDateInvalid = Number.isNaN(Date.parse(String(date)));
+    const isDateInvalid = Number.isNaN(new Date(date).valueOf());
 
     if (isDateInvalid) {
       throw new Error('Invalid Date.');
@@ -19,9 +19,23 @@ class DateHandler {
 }
 
 describe('Date handler', () => {
-  it('Should thows if an invalid date is provided.', () => {
+  it('should thows if an invalid date is provided.', () => {
     expect(() => new DateHandler('invalid-date')).toThrow(
       new Error('Invalid Date.'),
     );
+  });
+
+  it('should not throws if a valid or none date is provided.', () => {
+    const tests = [
+      '01/01/2022',
+      '2022-01-01',
+      '2022-01-01T00:00:00z',
+      new Date(),
+      1651280685731,
+    ];
+
+    tests.forEach((test) => {
+      expect(() => new DateHandler(test)).not.toThrow();
+    });
   });
 });
