2ca9b56f3e1b264a9c1313b5873f05e1acd2bc1f
indominuShot <luang193@gmail.com>
2022-04-30 21:17:15 -0300

    refactor: move method to their own files

--- __tests__/src/lib/date-handler/add-time.spec.ts
+++ __tests__/src/lib/date-handler/add-time.spec.ts
@@ -1,38 +1,5 @@
 import { DAY_IN_MS, HOUR_IN_MS, MINUTE_IN_MS } from '@/constants/time-units';
-
-type TTimeUnits = 'minute' | 'hour' | 'day' | 'week';
-type TAddTimeConfig = {
-  timeUnit: TTimeUnits;
-  qtd?: number;
-};
-
-const TIME_UNITS = {
-  minute: MINUTE_IN_MS,
-  hour: HOUR_IN_MS,
-  day: DAY_IN_MS,
-  week: DAY_IN_MS * 7,
-};
-
-function addTime(date: Date, config: TAddTimeConfig) {
-  if (!date) {
-    throw new Error('Missing date.');
-  }
-
-  if (!config || !config.timeUnit) {
-    throw new Error('Missing time unit.');
-  }
-
-  const { timeUnit, qtd } = config;
-
-  if (!TIME_UNITS[timeUnit]) {
-    throw new Error('Invalid time unit.');
-  }
-
-  const MIN_QTD = 1;
-  const timeToAdd = TIME_UNITS[timeUnit] * (qtd || MIN_QTD);
-
-  return new Date(date.getTime() + timeToAdd);
-}
+import { addTime, TTimeUnits } from '@/lib/date-handler/resources';
 
 describe('Add time', () => {
   it('should throws if no data is provided.', () => {
---
+++ src/lib/date-handler/resources/add-time.ts
@@ -0,0 +1,35 @@
+import { DAY_IN_MS, HOUR_IN_MS, MINUTE_IN_MS } from '@/constants/time-units';
+
+export type TTimeUnits = 'minute' | 'hour' | 'day' | 'week';
+export type TAddTimeConfig = {
+  timeUnit: TTimeUnits;
+  qtd?: number;
+};
+
+const TIME_UNITS = {
+  minute: MINUTE_IN_MS,
+  hour: HOUR_IN_MS,
+  day: DAY_IN_MS,
+  week: DAY_IN_MS * 7,
+};
+
+export function addTime(date: Date, config: TAddTimeConfig) {
+  if (!date) {
+    throw new Error('Missing date.');
+  }
+
+  if (!config || !config.timeUnit) {
+    throw new Error('Missing time unit.');
+  }
+
+  const { timeUnit, qtd } = config;
+
+  if (!TIME_UNITS[timeUnit]) {
+    throw new Error('Invalid time unit.');
+  }
+
+  const MIN_QTD = 1;
+  const timeToAdd = TIME_UNITS[timeUnit] * (qtd || MIN_QTD);
+
+  return new Date(date.getTime() + timeToAdd);
+}
