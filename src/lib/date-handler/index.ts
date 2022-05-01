bcc79cdd2922204337a59949d658e46365efea49
indominuShot <luang193@gmail.com>
2022-04-30 23:17:52 -0300

    feat:  - ensure sut always returns the correct timezone  - improve methods names  - add resources

--- src/lib/date-handler/index.ts
+++ src/lib/date-handler/index.ts
@@ -1,16 +1,19 @@
 import { HOUR_IN_MS } from '@/constants/time-units';
+import { resources } from './resources';
 
 export default class DateHandler {
-  private readonly defaultTimezone: string;
+  private readonly timezone: string;
   public date: Date;
 
   constructor(date?: string | Date | number) {
-    this.date = this.__initHandler(date);
-    this.defaultTimezone = 'America/Sao_Paulo';
+    this.date = this.__setInitialDate(date);
+    this.timezone = 'America/Sao_Paulo';
+
+    this.__initHandler();
   }
 
-  private __initHandler(date?: string | Date | number) {
-    if (!date) return this.__handleDefaultTimezone(new Date());
+  private __setInitialDate(date?: string | Date | number) {
+    if (!date) return this.__handleTimezone(new Date());
 
     const isDateInvalid = Number.isNaN(new Date(date).valueOf());
 
@@ -18,16 +21,32 @@ export default class DateHandler {
       throw new Error('Invalid Date.');
     }
 
-    return this.__handleDefaultTimezone(new Date(date));
+    return this.__handleTimezone(new Date(date));
   }
 
-  private __handleDefaultTimezone(date: Date) {
+  private __handleTimezone(date: Date) {
     const withUsTimezone = new Date(
-      date.toLocaleString('en-US', { timeZone: this.defaultTimezone }),
+      date.toLocaleString('en-US', { timeZone: this.timezone }),
     );
 
     const brUsDiffHours = 3;
 
     return new Date(withUsTimezone.getTime() - brUsDiffHours * HOUR_IN_MS);
+  }
+
+  private __initHandler() {
+    const methods = Object.keys(resources);
+
+    methods.forEach((method) => {
+      this[method] = () => {
+        const response = resources[method];
+
+        if (response instanceof Date) {
+          return this.__handleTimezone(response);
+        }
+
+        return response;
+      };
+    });
   }
 }
